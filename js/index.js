0fe1ad8006c58f99230c2ed35de7645cbcdb43e9
brellin <cekakid@gmail.com>
2019-03-12 00:37:14 -0500

    Changed one button to three and added login feature

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 // Get Variables
 let peeps = document.querySelectorAll('.person'),
-    cta = document.querySelector('.cta'),
+    cta = document.querySelector('.ctaholder'),
     header = document.querySelector('header'),
+    us = document.querySelector('.us'),
+    nav = document.querySelector('nav'),
     sticky = cta.offsetTop;
 
 // People Class
@@ -36,14 +38,35 @@ function engage() {
     if (window.pageYOffset >= sticky) {
         cta.classList.add('sticky');
         header.classList.add('nojump');
-        cta.innerHTML = '<div>Find A Listing</div>';
+        header.classList.add('no-jump');
     } else {
         cta.classList.remove('sticky');
         header.classList.remove('nojump');
-        cta.innerHTML = 'Find A Listing';
+        header.classList.remove('no-jump');
     }
 }
 
 // Push peeps to People and Plinks Class
 peeps.forEach(per => new People(per));
 peeps.forEach(per => new Plinks(per));
+
+// Add login feature
+let login = document.createElement('p');
+login.innerHTML = '<img src="images/dropdown.png">Log In';
+login.addEventListener('click', function () {
+    login.classList.toggle('dropdown');
+    let username = document.createElement('input'),
+        password = document.createElement('input'),
+        inputs = document.createElement('div');
+    username.placeholder = 'Username';
+    password.placeholder = 'Password';
+    inputs.appendChild(username);
+    inputs.appendChild(password);
+    nav.children.length === 4 ? nav.appendChild(inputs) : null;
+    let dd = nav.querySelector('div'),
+        ddc = dd.querySelectorAll('input');
+    dd.classList.toggle('drop');
+    dd.classList.contains('drop') ? ddc.forEach(dd => dd.classList.add('dd')) : ddc.forEach(dd => dd.classList.remove('dd'));
+    inputs.classList.add('drop');
+})
+nav.appendChild(login);
