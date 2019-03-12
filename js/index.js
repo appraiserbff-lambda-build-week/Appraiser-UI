92005d6e9ab70b656924539d85b8c2a086c77c83
brellin <cekakid@gmail.com>
2019-03-11 20:10:03 -0500

    Automated about id links

--- js/index.js
+++ js/index.js
@@ -8,10 +8,25 @@ let peeps = document.querySelectorAll('.person'),
 class People {
     constructor(person) {
         this.person = person;
-        this.name = this.person.querySelector('h3').innerText;
-        this.position = this.person.querySelector('h4').innerText;
         this.data = this.person.dataset.person;
         this.person.style.background = `linear-gradient( rgba(0, 0, 0, 0.25), rgba(50, 50, 50, 0.45)), url('images/${this.data}.jpg') scroll no-repeat center / contain`;
+    }
+}
+
+// Plinks Class
+class Plinks extends People {
+    constructor(person) {
+        super(person);
+        this.person = person;
+        this.name = this.person.querySelector('h3').innerText;
+        this.position = this.person.querySelector('h4').innerText;
+        this.id = this.person.id;
+        this.element = document.createElement('a');
+        this.element.href = `#${this.id}`;
+        this.element.innerText = this.name;
+        const us = document.querySelector('.us');
+        us.appendChild(this.element);
+        console.log(this.element);
     }
 }
 
@@ -29,5 +44,6 @@ function engage() {
     }
 }
 
-// Push peeps to People Class
+// Push peeps to People and Plinks Class
 peeps.forEach(per => new People(per));
+peeps.forEach(per => new Plinks(per));
